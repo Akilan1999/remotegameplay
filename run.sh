e9b511168db1dcc65ac6d73677d1387a28142c49
Akilan <me@akilan.io>
2023-02-19 23:42:37 +0000

    fixed run.sh for NAT traversal

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Starts server and runs it silently (i.e no output in the terminal)
-./remotegameplay -tls -addr 0.0.0.0:${2} > /dev/null 2>&1 &
+./remotegameplay -addr ${1} -port ${2} > /dev/null 2>&1 &
 # Starts chromium browser and runs it silently (i.e no output in the terminal)
 ./remotegameplay -headless -addr ${1} -port ${2} > /dev/null 2>&1 &
 # Lets the script sleep for 2 seconds
@@ -15,9 +15,9 @@ roomID=$(echo "$roomID" | tr -d '"')
 
 # Checks if the IP address is a IPV6 or IPV4 address
 if [ "$1" != "${1#*[0-9].[0-9]}" ]; then
-  echo "https://"${1}":${2}/?roomID="${roomID}
+  echo "http://"${1}":${2}/?roomID="${roomID}
 elif [ "$1" != "${1#*:[0-9a-fA-F]}" ]; then
-  echo "https://["${1}"]:${2}/?roomID="${roomID}
+  echo "http://["${1}"]:${2}/?roomID="${roomID}
 else
   echo "Unrecognized IP format '$1'"
 fi
